0b495f311e0280483e6f82dd2912c18f4b492df3
Ismail <souifiismail@gmail.com>
2026-05-30 18:22:15 +0100

    Feat: Add Singleton pattern implementation

--- src/main/java/org/mql/Main.java
+++ src/main/java/org/mql/Main.java
@@ -1,31 +1,31 @@
 package org.mql;
 
-import org.mql.model.AccountStatus;
 import org.mql.model.AccountType;
 import org.mql.model.BankAccount;
-import org.mql.model.BankDirector;
-import org.mql.repository.AccountRepository;
 import org.mql.repository.AccountRepositoryIDefault;
 import org.mql.util.JsonSerializer;
 
+import java.io.IOException;
 import java.util.List;
-import java.util.function.Predicate;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    public Main() {
-        exp01();
+    public Main() throws IOException {
+        // exp01();
+        exp02Singleton();
     }
 
-    void exp01(){
+    void exp01() {
 
         JsonSerializer<BankAccount> bankAccountJsonSerializer = new JsonSerializer<>();
 
 
-        AccountRepositoryIDefault accountRepositoryIDefault = new AccountRepositoryIDefault();
-        accountRepositoryIDefault.populateData();
+        AccountRepositoryIDefault accountRepositoryIDefault = AccountRepositoryIDefault.getInstance();
+
+
+        //accountRepositoryIDefault.populateData();
 
 //        List<BankAccount> accounts = accountRepositoryIDefault.findAll();
 //        accounts.forEach(bankAccountJsonSerializer::toJson);
@@ -36,14 +36,14 @@ public class Main {
 //
 //
 //        accounts.stream()
-//                .map(bankAccountJsonSerializer::toJson) // pour chaque obj de bankaccount je vais applique la method tojson
+//                .map(bankAccountJsonSerializer::toJson) // pour chaque obj de bankaccount je vais appliquer la method tojson
 //                .forEach(System.out::println);
 
         System.out.println("==================================");
 
         BankAccount account = accountRepositoryIDefault.findById(1L).orElse(null);
 
-        if(account != null){
+        if (account != null) {
             System.out.println(bankAccountJsonSerializer.toJson(account));
         }
 
@@ -56,19 +56,45 @@ public class Main {
 //            }
 //        });
 
-        List<BankAccount> accountsSearch = accountRepositoryIDefault
-                .searchAccounts(bankAccount -> bankAccount.getType().equals(AccountType.CURRENT_ACCOUNT) && bankAccount.getBalance()>1000);
+        List<BankAccount> accountsSearch = accountRepositoryIDefault.searchAccounts(bankAccount -> bankAccount.getType().equals(AccountType.CURRENT_ACCOUNT) && bankAccount.getBalance() > 1000);
 
         accountsSearch.forEach(bankAccountJsonSerializer::toJson);
 
-        accountsSearch.stream()
-                .map(bankAccountJsonSerializer::toJson) // pour chaque obj de bankaccount je vais applique la method tojson
+        accountsSearch.stream().map(bankAccountJsonSerializer::toJson) // pour chaque obj de bankaccount je vais appliquer la method tojson
                 .forEach(System.out::println);
 
 
     }
 
-    static void main() {
+    void exp02Singleton() throws IOException {
+        JsonSerializer<BankAccount> bankAccountJsonSerializer = new JsonSerializer<>();
+
+
+        AccountRepositoryIDefault accountRepositoryIDefault = AccountRepositoryIDefault.getInstance();
+
+        for (int i = 0; i < 10; i++) {
+            /*new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+                }
+            }).start();*/
+
+            new Thread(() -> {
+                accountRepositoryIDefault.populateData();
+            }).start();
+        }
+        System.out.println("Tape a character : ");
+        System.in.read();
+
+
+        List<BankAccount> accountsSearch = accountRepositoryIDefault.findAll();
+
+        accountsSearch.stream().map(bankAccountJsonSerializer::toJson) // pour chaque obj de bankaccount je vais appliquer la method tojson
+                .forEach(System.out::println);
+    }
+
+    static void main() throws IOException {
         new Main();
     }
 }
--- src/main/java/org/mql/repository/AccountRepositoryIDefault.java
+++ src/main/java/org/mql/repository/AccountRepositoryIDefault.java
@@ -14,17 +14,67 @@ import java.util.stream.Collectors;
 
 public class AccountRepositoryIDefault implements AccountRepository {
 
-    private Map<Long , BankAccount > accounts = new HashMap<>();
+    // Singleton Implementation
+    // private static final AccountRepositoryIDefault accountRepository;
+    private static AccountRepositoryIDefault accountRepository;
+
+    private Map<Long, BankAccount> accounts = new HashMap<>();
 
     private long accountsCount = 0;
 
+    static {
+        System.out.println("Singleton instantiation");
+        accountRepository = new AccountRepositoryIDefault();
+    }
+
+    private AccountRepositoryIDefault() {
+
+    }
+
+    public static AccountRepositoryIDefault getInstance() {
+//        if (accountRepository == null) {
+//            System.out.println("Singleton instantiation");
+//            accountRepository = new AccountRepositoryIDefault();
+//            //accountRepository.populateData();
+//        }
+        return accountRepository;
+    }
+
+
+//    @Override
+//    public BankAccount save(BankAccount bankAccount) {
+//
+//        // First solution synchronize an bloc :
+//        Long accountId;
+//        synchronized (this){
+//            accountId = ++accountsCount; // Critical zone
+//            bankAccount.setAccountId(accountId);
+//            accounts.put(accountId , bankAccount);
+//        }
+//
+//        //Long accountId = accountId = ++accountsCount;
+
+    /// /        bankAccount.setAccountId(accountId);
+    /// /        accounts.put(accountId , bankAccount);
+//
+//        return bankAccount;
+//    }
+
+    // Third solution
     @Override
     public BankAccount save(BankAccount bankAccount) {
-        Long accountId = ++accountsCount;
+        Long accountId;
+        synchronized (this) {
+            accountId = ++accountsCount;
+        }
         bankAccount.setAccountId(accountId);
-        accounts.put(accountId , bankAccount);
+        synchronized (this) {
+            accounts.put(accountId, bankAccount);
+
+        }
         return bankAccount;
     }
+
 
     @Override
     public List<BankAccount> findAll() {
@@ -35,7 +85,7 @@ public class AccountRepositoryIDefault implements AccountRepository {
     @Override
     public Optional<BankAccount> findById(Long id) {
         BankAccount account = accounts.get(id);
-        if(account==null)
+        if (account == null)
             return Optional.empty();
         else
             return Optional.of(account);
@@ -50,7 +100,7 @@ public class AccountRepositoryIDefault implements AccountRepository {
 
     @Override
     public BankAccount update(BankAccount account) {
-        accounts.put(account.getAccountId() , account);
+        accounts.put(account.getAccountId(), account);
         return account;
     }
 
@@ -60,17 +110,22 @@ public class AccountRepositoryIDefault implements AccountRepository {
 
     }
 
-    public void populateData(){
+    public synchronized void populateData() {
+        BankAccount bankAccount = null;
         for (int i = 0; i < 10; i++) {
-            BankAccount bankAccount = BankDirector.accountBuilder()
-                    .accountId(1L)
-                    .currency(Math.random()>0.5?"MAD":"USD")
-                    .status(Math.random()>0.5?AccountStatus.CREATED:AccountStatus.ACTIVATED)
-                    .type(Math.random()>0.5?AccountType.SAVING_ACCOUNT:AccountType.CURRENT_ACCOUNT)
-                    .balance(10000+Math.random()*90000)
+            bankAccount = BankDirector.accountBuilder()
+                    .currency(Math.random() > 0.5 ? "MAD" : "USD")
+                    .status(Math.random() > 0.5 ? AccountStatus.CREATED : AccountStatus.ACTIVATED)
+                    .type(Math.random() > 0.5 ? AccountType.SAVING_ACCOUNT : AccountType.CURRENT_ACCOUNT)
+                    .balance(10000 + Math.random() * 90000)
                     .build();
             save(bankAccount);
 
         }
+        System.out.println("*************************************** ");
+        System.out.println("Nom du Thread : " + Thread.currentThread().getName());
+        System.out.println("Account count : " + accountsCount);
+        System.out.println("Size : " + accounts.values().size());
+
     }
 }
